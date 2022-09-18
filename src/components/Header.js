ea99e2b26528fe94b1af826a50d9113e883f97d1
Artem Dimoglov <adimoglov@gmail.com>
2022-09-19 00:03:02 +0300

    front - done, now settin up Firestore

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,33 @@
 import React from "react";
 import Cart from "./Cart";
-import "./Header.css";
+import {useDispatch} from "react-redux";
+import {authActions} from "../store/auth-slice";
+
 const Header = () => {
+  const dispatch = useDispatch();
+
+  const logoutHandler = () => {
+    dispatch(authActions.logout());
+  };
+
   return (
     <header>
-      <nav className="header-nav">
-        <ul className="header-ul">
-          <li>
-            <h2
-              className="header-h2"
-              style={{ fontFamily: "monospace", fontSize: "30px" }}
-            >
-              Redux Shopping App
-            </h2>
+      <nav className = "sticky shadow-lg ">
+        <div className = "">
+        <ul className = "flex items-center justify-between">
+          <li className = "mr-[50%]">
+            <h2 className = "text-[30px] font-mono">Redux Shopping App</h2>
           </li>
           <li>
             <Cart />
           </li>
+          <li>
+            <button onClick = {logoutHandler}
+                    className = "mb-5 border-none px-4 py-1.5 rounded-full bg-orange-500 hover:bg-orange-700 ">Logout
+            </button>
+          </li>
         </ul>
+        </div>
       </nav>
     </header>
   );
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,20 +2,31 @@ import React from "react";
 import Header from "./Header";
 import Products from "./Products";
 import "./Layout.css";
+import CartItems from "./CartItems";
+import { useSelector } from "react-redux";
+
 const Layout = () => {
-  let total = 100;
+  const itemsList = useSelector((state) => state.cart.itemsList);
+
+  let total = 0;
+  itemsList.forEach((item) => {
+    total += item.totalPrice;
+  });
+
+  const showCart = useSelector((state) => state.cart.showCart);
 
   return (
-    <React.Fragment>
-      <div className="layout">
-        <Header />
-        <Products />
-        <div className="total-price">
-          <h3>Total: ${total}</h3>
-          <button className="orderBtn">Place Order</button>
-        </div>{" "}
-      </div>
-    </React.Fragment>
+      <React.Fragment>
+        <div className="flex flex-1 flex-col">
+          <Header />
+          <Products />
+          {showCart && <CartItems />}
+          <div className="total-price">
+            <h3>Total: ${total}</h3>
+            <button className="orderBtn">Place Order</button>
+          </div>{" "}
+        </div>
+      </React.Fragment>
   );
 };
 
