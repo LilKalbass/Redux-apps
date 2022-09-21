cc6cfdd07c3d0a0c39ec349479a11f587785c0a0
Artem Dimoglov <adimoglov@gmail.com>
2022-09-21 06:17:32 +0300

    Added firestore

---
+++ src/store/cart-actions.js
@@ -0,0 +1,59 @@
+import { cartActions } from "./cart-slice";
+import { uiActions } from "./ui-slice";
+
+export const fetchData = () => {
+    return (dispatch) => {
+        fetch("https://redux-shoppin-cart-default-rtdb.firebaseio.com/cartItems.json")
+            .then((res) => res.json())
+            .then((result) => {
+                dispatch(cartActions.replaceData(result))
+                console.log(result)
+            })
+            .catch((err) => {
+                dispatch(
+                    uiActions.showNotification({
+                        open: true,
+                        message: "fetching from DB failed",
+                        type: "error",
+                    })
+                );
+            })
+    };
+};
+
+export const sendCartData = (cart) => {
+    return (dispatch) => {
+        dispatch(
+            uiActions.showNotification({
+                open: true,
+                message: "Sending Request To Database!",
+                type: "warning",
+            })
+        );
+        fetch("https://redux-shoppin-cart-default-rtdb.firebaseio.com/cartItems.json", {
+            method: "PUT",
+            body: JSON.stringify(cart)
+        })
+            .then((res) => {
+                 console.log(res);
+                return res.json()
+
+            })
+            .then((result) => {
+                dispatch(uiActions.showNotification({
+                    open: true,
+                    message: "Success",
+                    type: "success"
+                }))
+                console.log(result);
+            })
+            .catch((err) => {
+                dispatch(uiActions.showNotification({
+                    open: true,
+                    message: "Fail",
+                    type: "error"
+                }))
+                // console.log(err);
+            })
+    }
+}
--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,14 +1,21 @@
 import {createSlice} from "@reduxjs/toolkit";
+import {uiActions} from "./ui-slice";
 
 const cartSlice = createSlice({
     name: "cart",
     initialState: {
         itemsList: [],
         totalQuantity: 0,
-        showCart: false
+        showCart: false,
+        changed: false
     },
     reducers: {
+        replaceData(state, action) {
+            state.totalQuantity = action.payload.totalQuantity;
+            state.itemsList = action.payload.itemsList??[];
+        },
         addToCart(state, action) {
+            state.changed = true;
             const newItem = action.payload;
             const existingItem = state.itemsList.find((item) => item.id === newItem.id);
 
@@ -28,6 +35,7 @@ const cartSlice = createSlice({
             }
         },
         removeFromCart(state, action) {
+            state.changed = true;
             const id = action.payload;
             const existingItem = state.itemsList.find(item => item.id === id);
 
